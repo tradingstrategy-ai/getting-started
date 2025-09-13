e34d67627f49eb32de0caff5ff491cd316f9c287
Ken Kunz <kennethkunz@gmail.com>
2025-09-13 09:35:48 -0500

    Limit tests to notebooks that are known to run correctly

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -4,21 +4,27 @@ import pytest
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 
-NOTEBOOK_GLOB_PATTERN = "notebooks/single-backtest/*.ipynb"
+NOTEBOOK_GLOB_PATTERN = "notebooks/**/*.ipynb"
 
-def is_grid_search(notebook_path: str):
-    """Check if notebook contains grid search functionality."""
+# Notebooks containing any of these strings will be skipped
+# - Fix issues in these notebooks (related to the skip strings) to enable them to run successfully
+# - Notebooks requiring perform_gride_search and prefilter- scripts may need to be run manually due to long runtimes
+SKIP_NOTEBOOK_STRINGS = ["perform_grid_search", "prefilter-", "create_binance_universe", "df_trend_polygon", "polygon_token_list", "filter_scams", "demeter"]
+
+def contains_code(notebook_path: str, search_strings: list[str]) -> bool:
+    """Check if notebook contains any code strings."""
     with open(notebook_path) as f:
         nb = nbformat.read(f, as_version=4)
         for cell in nb.cells:
-            if cell.cell_type == "code" and "perform_grid_search" in cell.source:
-                return True
+            if cell.cell_type == "code":
+                if any(search_string in cell.source for search_string in search_strings):
+                    return True
     return False
 
 def get_notebooks_to_test():
-    """Get list of notebooks to test (excluding grid search notebooks)."""
+    """Get list of notebooks to test (excluding notebooks containing "skip" strings)."""
     all_notebooks = glob.glob(NOTEBOOK_GLOB_PATTERN, recursive=True)
-    return [nb for nb in all_notebooks if not is_grid_search(nb)]
+    return [nb for nb in all_notebooks if not contains_code(nb, SKIP_NOTEBOOK_STRINGS)]
 
 def run_notebook(notebook_path: str):
     """Execute a notebook and return True if successful."""
