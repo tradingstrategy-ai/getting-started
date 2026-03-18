6e58d19cbf657bed748ffe95984234307b00895c
Mikko Ohtamaa <mikko@redinnovation.com>
2026-03-18 17:02:06 +0100

    Add sliding in and out of positions with max trade size

--- getting_started/__init__.py
+++ getting_started/__init__.py
@@ -1,2 +1,5 @@
-# Dummy Python module for now
+"""Compatibility exports for notebook helpers now hosted in tradeexecutor."""
 
+from .curator import *  # noqa: F403
+from .hyperliquid_vault_universe import *  # noqa: F403
+from .vault_universe_creation import *  # noqa: F403
